1567d99e3990b934f732531dbb1c41bd015c11d3
james <james@bubbleye.com>
2023-11-13 16:24:49 +0800

    Add superset db and create sources

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 
 ## Druid: Setup
 
+# Kafka 
+`./bin/kafka-topics.sh --create --topic impressions --bootstrap-server localhost:9092`
+`./bin/kafka-topics.sh --create --topic clicks --bootstrap-server localhost:9092`
+
 https://druid.apache.org/docs/latest/tutorials/
 
 ## Druid: User (admin)
@@ -17,5 +21,10 @@ https://druid.apache.org/docs/latest/operations/security-overview
 
 set `export FLASK_APP=superset`
 set `export SUPERSET_CONFIG_PATH=superset/superset_config.py`
+
+`superset db upgrade`
+`superset fab create-admin` > Setup your username and password for the superset admin login
+`superset init`
+`superset run -p 8088 --with-threads --reload`
 
 1. Add database `druid://admin:password1@localhost:8888/druid/v2/sql`
--- api_app/controllers/postbacks.py
+++ api_app/controllers/postbacks.py
@@ -1,11 +1,9 @@
-from litestar import Controller, get
+from litestar import Controller, get, Request
 from litestar.exceptions import HTTPException
 
 from confluent_kafka import KafkaException, Producer
 
 import json 
-
-
 
 
 
@@ -35,10 +33,10 @@ def delivery_report(err, msg):
 
 
 class PostbackController(Controller):
-    path = "/impressions"
+    path = "collect"
 
-    @get(path="/{app:str}")
-    async def impressions(self, app: str, c:str, c_id:str | None = None, ad:str | None = None) -> None:
+    @get(path="impression/{app:str}")
+    async def impressions(self, request:Request, app: str, source:str, c:str, c_id:str | None = None, ad:str | None = None, idfa:str|None=None) -> None:
         """
         Handles a GET request for a list of apps
 
@@ -50,13 +48,41 @@ class PostbackController(Controller):
         """
         logger.info(f"{self.path} start")
 
+        client_host = request.client.host
 
-        data = {"app":app, "campaign_name":c, "campaign_id": c_id, "ad":ad}
+        data = {"source":source, "app":app, "campaign_name":c, "campaign_id": c_id, "ad":ad, "client_ip":client_host}
 
         try:
             enc_data = json.dumps(data).encode('utf-8')
             logger.info(f'data={enc_data}')
-            producer.produce("mytopic", value=enc_data)
+            producer.produce("impressions", value=enc_data)
+            producer.poll(0)
+            logger.info(f'data={enc_data} success!')
+        except KafkaException as ex:
+            logger.error({"status": "error", "message": str(ex)})
+            raise HTTPException(status_code=500, detail=ex.args[0].str())
+
+    @get(path="click/{app:str}")
+    async def clicks(self, request:Request, app: str, source:str, c:str, c_id:str | None = None, ad:str | None = None, idfa:str|None =None) -> None:
+        """
+        Handles a GET request for a list of apps
+
+        Args:
+            app:app
+
+        Returns:
+            A dictionary representation of the list of apps for homepage
+        """
+        logger.info(f"{self.path} start")
+
+        client_host = request.client.host
+
+        data = {"source":source, "app":app, "campaign_name":c, "campaign_id": c_id, "ad":ad, "client_ip":client_host}
+
+        try:
+            enc_data = json.dumps(data).encode('utf-8')
+            logger.info(f'data={enc_data}')
+            producer.produce("clicks", value=enc_data)
             producer.poll(0)
             logger.info(f'data={enc_data} success!')
         except KafkaException as ex:
