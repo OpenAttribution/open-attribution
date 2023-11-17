6eb53f569c9ca88665136211b6f87923adc2fb91
james <james@bubbleye.com>
2023-11-17 11:24:52 +0800

    Checking in while working on a launcher.sh file, added shfmt to pre-commit

--- launcher.sh
+++ launcher.sh
@@ -1,35 +1,71 @@
 #!/bin/bash
 
-export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64"
-export PATH="$JAVA_HOME/bin:$PATH"
+source ./local.conf
 
+if ! source ./local.conf; then
+	echo "Error: Unable to source local.conf. Check you are in open-attribution directory. Copy with: cp example_local.conf local.conf"
+	exit 1
+fi
+
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run as root"
+	exit
+fi
+
+# This function assembles the kubelet systemd service file and starts it
+# using systemctl.
+function start-druid {
+	echo "Start druid service"
+	echo "druid_dir: $DRUID_DIR"
+	# Write the systemd service file for kubelet.
+	cat <<EOF >/etc/systemd/system/open-attribution-druid.service
+[Unit]
+Description=Open Attribution Druid service
+After=network.target
+
+[Service]
+User=james
+Group=james
+ExecStart=${DRUID_DIR}
+ExecReload=/bin/kill -s HUP $MAINPID
+Restart=on-failure
+KillMode=mixed
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+	systemctl daemon-reload
+	systemctl start open-attribution-druid.service
+
+}
 
 app_dir=$(pwd)
 echo "setting app_dir to ${app_dir}"
 my_link="/etc/systemd/system/open-attribution-druid.service"
 
-
-if [ -L ${my_link} ] ; then
-   if [ -e ${my_link} ] ; then
-      echo "Good link"
-   else
-      echo "Broken link"
-	sudo rm ${my_link}
-	sudo ln -s ${app_dir}/scripts/open-attribution-druid.service ${my_link}
-   	echo "Druid service added ${my_link}"
-   fi
-elif [ -e ${my_link} ] ; then
-   echo "Not a link"
-   sudo ln -s ${app_dir}/scripts/open-attribution-druid.service ${my_link}
-   echo "Druid service added ${my_link}"
+if [ -f "${my_link}" ]; then
+	echo "Druid service already exists ${my_link}"
+	start-druid
 else
-   echo "Missing"
-   sudo ln -s ${app_dir}/scripts/open-attribution-druid.service ${my_link}
-   echo "Druid service added ${my_link}"
+	echo "Create druid service ${my_link}"
+	start-druid
+	echo "Druid service added ${my_link}"
 fi
 
-sudo systemctl daemon-reload
-
-
-sudo systemctl start open-attribution-druid.service
-
+systemctl is-active --quiet open-attribution-druid.service >/dev/null 2>&1
+status=$?
+if [ $status -eq 0 ]; then
+	echo "open-attribution-druid is running normally. check http://localhost:8888"
+elif [ $status -eq 3 ]; then
+	echo "open-attribution-druid.service is not running (inactive)."
+	journalctl -u open-attribution-druid.service | tail -n 10
+elif [ $status -eq 4 ]; then
+	echo "open-attribution-druid encountered an error (failed)."
+	journalctl -u open-attribution-druid.service | tail -n 10
+else
+	echo "Status check returned an unexpected status code: $status"
+fi
--- tmuxlauncher.sh
+++ tmuxlauncher.sh
@@ -3,7 +3,6 @@ port=$1
 app=$2
 
 session="openattribution"
-randomsuffix="$(LC_ALL=C tr -dc a-z0-9 </dev/urandom | head -c 4)"
 
 WAYDROID_LOG_FILE=/var/lib/waydroid/waydroid.log
 
@@ -11,54 +10,52 @@ sudo systemctl stop waydroid-container.service
 sudo systemctl start waydroid-container.service
 
 if [[ -n $(pgrep tmux) ]]; then
-    tmuxrunning=true
+	tmuxrunning=true
 else
-    tmuxrunning=false
+	tmuxrunning=false
 fi
 
 if [ "$tmuxrunning" = true ]; then
-    echo "tmux already up = $tmuxrunning"
-    # Check if the session exists, discarding output
-    # We can check $? for the exit status (zero for success, non-zero for failure)
-    tmux has-session -t $session 2>/dev/null
-    # kill session, temp
-    if [[ $? = 0 ]]; then
-        echo "Found running tmux $session"
-        dolaunchsess=false
-        #tmux kill-session -t $session
-    else
-        dolaunchsess=true
-        echo "tmux up but can't find $session"
-    fi
+	echo "tmux already up = $tmuxrunning"
+	# Check if the session exists, discarding output
+	# We can check $? for the exit status (zero for success, non-zero for failure)
+	#tmux has-session -t $session 2>/dev/null
+	# kill session, temp
+	#if [[ $? == 0 ]]; then
+	if tmux has-session -t $session == 0; then
+		echo "Found running tmux $session"
+		dolaunchsess=false
+		#tmux kill-session -t $session
+	else
+		dolaunchsess=true
+		echo "tmux up but can't find $session"
+	fi
 else
-    echo "tmux not open, launch new"
-    dolaunchsess=true
+	echo "tmux not open, launch new"
+	dolaunchsess=true
 fi
 
 if [ "$dolaunchsess" = true ]; then
-    echo "Launching new session"
-    # Set up your session
-    tmux new-session -d -s $session
-    # Initial: Top Left
-    tmux send-keys -t $session "waydroid session start" Enter
-    # Top Right
-    tmux split-window -h -t $session
-    tmux send-keys -t $session "~/app-spy/proxysetup.sh $port -w $logfile" Enter
-    # Bottom Right
-    #tmux split-window -v -p 50 -t $session
-    # Bottom Left
-    tmux select-pane -t $session:0.0
-    tmux split-window -v -p 50 -t $session
-    tmux send-keys -t $session "sleep 1" Enter
-    tmux send-keys -t $session "tail -f ${WAYDROID_LOG_FILE} | grep --line-buffered 'user 0 is ready' | while read ; do ~/app-spy/check_app.sh -s $app && echo 'Launch Waydroid UI..'. && waydroid app launch $app ; done" Enter
-    tmux send-keys -t $session "~/app-spy/check_app.sh -s $app" Enter
-    # Put cursor to proxy pane which will ask for sudo
-    tmux select-pane -t $session:0.2
+	echo "Launching new session"
+	# Set up your session
+	tmux new-session -d -s $session
+	# Initial: Top Left
+	tmux send-keys -t $session "waydroid session start" Enter
+	# Top Right
+	tmux split-window -h -t $session
+	tmux send-keys -t $session "proxysetup.sh $port -w " Enter
+	# Bottom Right
+	#tmux split-window -v -p 50 -t $session
+	# Bottom Left
+	tmux select-pane -t $session:0.0
+	tmux split-window -v -p 50 -t $session
+	tmux send-keys -t $session "sleep 1" Enter
+	tmux send-keys -t $session "tail -f ${WAYDROID_LOG_FILE} | grep --line-buffered 'user 0 is ready' | while read ; do check_app.sh -s  && echo 'Launch Waydroid UI..'. && waydroid app launch $app ; done" Enter
+	tmux send-keys -t $session "/app-spy/check_app.sh -s $app" Enter
+	# Put cursor to proxy pane which will ask for sudo
+	tmux select-pane -t $session:0.2
 else
-    echo "Attach existing new session"
+	echo "Attach existing new session"
 fi
 
 tmux att -t $session
-
-
-
