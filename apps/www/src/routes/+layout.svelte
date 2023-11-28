dd17d3b373a039526071d59754cf4e5874720eb9
james <james@bubbleye.com>
2023-11-28 22:21:06 +0800

    Rough draft of home page of site

--- apps/www/src/routes/+layout.svelte
+++ apps/www/src/routes/+layout.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
-	import '../app.postcss';
+	import NavTabs from '$lib/NavTabs.svelte';
+import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
 <!-- App Shell -->
 <AppShell>
+	
+	<!-- Large only header -->
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
+			<AppBar
+			gridColumns="grid-cols-[1fr_0_1fr] md:grid-cols-[0.4fr_1fr_0.3fr]"
+			slotLead="p-2"
+			slotTrail="p-2"
+			spacing="space-y-0"
+			padding="p-0"
+		>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Open Attribution</strong>
+			<strong class=" ml-4 text-xl md:text-2xl uppercase">Open Attribution</strong>
 			</svelte:fragment>
+
+
+			<div class="hidden lg:inline-flex">
+				<NavTabs></NavTabs>
+			</div>
+
+
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
@@ -21,8 +37,34 @@
 					GitHub
 				</a>
 			</svelte:fragment>
+
+
+
 		</AppBar>
 	</svelte:fragment>
+
+	<!--  Mobile only footer -->
+	<svelte:fragment slot="footer">
+		<AppBar
+			slotLead="p-0"
+			slotTrail="p-2"
+			spacing="space-y-0"
+			padding="p-0"
+			gap="gap-0"
+			class="lg:hidden"
+			gridColumns="grid-cols-[auto_1fr_auto]"
+		>
+			<svelte:fragment slot="lead">
+				<div class="inline-flex">
+					<NavTabs />
+				</div>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
 	<!-- Page Route Content -->
 	<slot />
 </AppShell>
+
+
+
--- apps/www/src/routes/+page.svelte
+++ apps/www/src/routes/+page.svelte
@@ -1,1 +1,19 @@
-<h1 class="h1">Hello World</h1>
+
+<div class="p-2">
+
+<div class="card mx-8 lg:mx-96 my-12 lg:my-44 flex items-center justify-center text-center">
+<div>
+
+<h1 class="h1 text-center pt-12">Own Your Data</h1>
+<h1 class="h1 text-center p-4 lg:p-8">Control Your Advertising Attribution</h1>
+<h3 class="h3 text-center p-2 lg:p-4">Open Attribution is an open source MMP alternative. Fully host and own your data by overseeing it's storage, manage your data pipeline and control your advertising campaign attribution rules.</h3>
+</div>
+
+<div class="flex items-center gap-2">
+<button type="button" class="btn variant-outline-secondary mr-4 justify-center">Learn More</button>
+<button type="button" class="btn variant-outline-primary">Documentation</button>
+</div>
+</div>
+</div>
+<div class="p-4"></div>
+<div class="card p-4">Basic</div>
