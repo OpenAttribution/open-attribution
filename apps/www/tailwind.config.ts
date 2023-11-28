b5cac569d5fca7586a5211102df147c96545edfe
james <james@bubbleye.com>
2023-11-28 16:18:44 +0800

    Add base skeleton + tailwind app page

---
+++ apps/www/tailwind.config.ts
@@ -0,0 +1,21 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import { skeleton } from '@skeletonlabs/tw-plugin'
+import { oaTheme } from './oa-theme'
+
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		skeleton({
+			themes: {
+				custom: [
+				oaTheme
+			]
+			},
+		}),
+	],
+} satisfies Config;
