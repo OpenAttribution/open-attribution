1c1ffdd99d0d3cb47fd35902d7f3ab15927037c9
james <james@bubbleye.com>
2023-11-29 21:56:54 +0800

    minor fix

--- apps/www/src/routes/+page.svelte
+++ apps/www/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script type="ts">
 	import { goto } from '$app/navigation';
-	import myWarningTriangle from '$lib/assets/triangle-warning.svg?raw'
+	import myWarningTriangle from '$lib/assets/triangle-warning.svg?raw';
 	function redirectToProduct() {
 		goto('/product');
 	}
@@ -9,38 +9,39 @@
 	}
 </script>
 
-
 <aside class="alert variant-filled-secondary p-4 mx-8 lg:mx-96 mt-8">
 	{@html myWarningTriangle}
 	<div class="alert-message">
 		<h3 class="h3">Early Alpha</h3>
-		<p>This open source project is in the very early alpha stages. If you're interested please reach out on GitHub, LinkedIn or Discord</p>
+		<p>
+			This open source project is in the very early alpha stages. If you're interested please reach
+			out on GitHub, LinkedIn or Discord
+		</p>
 	</div>
 </aside>
 
+<div class="card mx-8 lg:mx-96 my-12 lg:mt-8 flex items-center justify-center text-center">
+	<div class="py-12">
+		<h1 class="h1 text-center">Own Your Data</h1>
+		<h1 class="h1 text-center text-secondary-500">Control Your Ad Attribution</h1>
+		<h4 class="h4 text-center p-2 lg:p-4">
+			Open Attribution is an open source MMP alternative. Fully host and own your data by overseeing
+			it's storage, manage your data pipeline and control your advertising campaign attribution
+			rules.
+		</h4>
 
-	<div class="card mx-8 lg:mx-96 my-12 lg:mt-8 flex items-center justify-center text-center">
-		<div class="py-12">
-			<h1 class="h1 text-center">Own Your Data</h1>
-			<h1 class="h1 text-center text-secondary-500">Control Your Ad Attribution</h1>
-			<h4 class="h4 text-center p-2 lg:p-4">
-				Open Attribution is an open source MMP alternative. Fully host and own your data by
-				overseeing it's storage, manage your data pipeline and control your advertising campaign
-				attribution rules.
-			</h4>
-
-			<div class="flex justify-center gap-2">
-				<button
-					type="button"
-					class="btn variant-outline-secondary mr-4 justify-center"
-					on:click={redirectToProduct}>Learn More</button
-				>
-				<button type="button" class="btn variant-outline-primary" on:click={redirectToDocs}
-					>Documentation</button
-				>
-			</div>
+		<div class="flex justify-center gap-2">
+			<button
+				type="button"
+				class="btn variant-outline-secondary mr-4 justify-center"
+				on:click={redirectToProduct}>Learn More</button
+			>
+			<button type="button" class="btn variant-outline-primary" on:click={redirectToDocs}
+				>Documentation</button
+			>
 		</div>
 	</div>
+</div>
 
 <div class="grid grid-cols-2 gap-4 lg:gap-16 p-4 lg:px-48">
 	<div class="card p-4">
