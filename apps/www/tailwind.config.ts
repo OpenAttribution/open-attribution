c305aedbe328532f3e4f7fdf259c81ea69ca17e4
james <james@bubbleye.com>
2023-12-01 16:00:30 +0800

    move theme to shared

--- apps/www/tailwind.config.ts
+++ apps/www/tailwind.config.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
 import { skeleton } from '@skeletonlabs/tw-plugin';
-import { oaTheme } from './oa-theme';
+import { oaTheme } from '../shared/oa-theme';
 
 export default {
 	darkMode: 'class',
