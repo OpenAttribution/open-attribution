62a6c8c3a8f41027873edcdf2f3e43ce28e026be
james <james@bubbleye.com>
2023-12-08 13:35:35 -0800

    Delete pricing page, way too early for that

--- apps/www/src/routes/pricing/+page.svelte
+++
@@ -1,5 +0,0 @@
-<h1 class="h1">Pricing</h1>
-
-As this project is very early stage, the price tier currently is self-hosted for<strong
-	>free.</strong
-> A beta with hosted attribution for general or testing use will be available in Q1 2024.
