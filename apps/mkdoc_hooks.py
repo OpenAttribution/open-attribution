59d596d0b3165ae6f81b62e1e0db4734110ea387
james <jamesoclaire@gmail.com>
2024-03-05 13:23:13 -0800

    Added hooks

--- apps/mkdoc_hooks.py
+++ apps/mkdoc_hooks.py
@@ -1,10 +1,10 @@
 # type: ignore
-# ruff: noqa
 """Make changes to mkdocs via hooks."""
 
 import re
 
 
 def on_post_page(output: str, **kwargs) -> str:
+    # ruff: noqa
     """Override the mkdocs post_page function."""
     return re.sub(r'href="([^"]+)"', r'href="\1" target="_top"', output)
