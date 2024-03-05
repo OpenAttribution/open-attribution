03181bb682c10894c3a3109373f61826cead68cb
james <jamesoclaire@gmail.com>
2024-03-05 14:23:08 -0800

    Working, blog and docs

---
+++ apps/www/src/routes/blog/[...file]/+page.server.ts
@@ -0,0 +1,30 @@
+export const csr = false;
+
+import { PUBLIC_DOCS_HOST } from '$env/static/public';
+
+async function fetchContent(page) {
+	let htmlContent;
+	if (page) {
+		// Ensure the path correctly points to where your HTML files are located.
+		var mypath = `${PUBLIC_DOCS_HOST}/docs${page.pathname}index.html`;
+		console.log(`LETS GO ${mypath}`);
+		const response = await fetch(mypath);
+		if (response.ok) {
+			htmlContent = await response.text();
+		} else {
+			htmlContent = 'Page not found.';
+		}
+	}
+	htmlContent = `${htmlContent}`;
+	// console.log(`GOT CONTENT: ${htmlContent}`)
+	return htmlContent;
+}
+
+export async function load({ url }) {
+	const myhtml = await fetchContent(url);
+	return {
+		props: {
+			myblog: myhtml
+		}
+	};
+}
