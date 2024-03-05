1602665d6930e5fc006be523332883b95531a8e2
james <jamesoclaire@gmail.com>
2024-03-05 13:22:01 -0800

    Added hooks

---
+++ apps/mkdoc_hooks.py
@@ -0,0 +1,10 @@
+# type: ignore
+"""Make changes to mkdocs via hooks."""
+
+import re
+
+
+# ruff: noqa
+def on_post_page(output: str, **kwargs) -> str:
+    """Override the mkdocs post_page function."""
+    return re.sub(r'href="([^"]+)"', r'href="\1" target="_top"', output)
--- apps/www/src/routes/docs/[...file]/+page.server.ts
+++ apps/www/src/routes/docs/[...file]/+page.server.ts
@@ -4,7 +4,6 @@ export const csr = false;
 async function fetchContent(page) {
 	let htmlContent;
 	if (page) {
-		// Construct the URL dynamically based on the page parameter.
 		// Ensure the path correctly points to where your HTML files are located.
 		var mypath = `http://localhost:5173${page.pathname}index.html`;
 		console.log(`LETS GO ${mypath}`);
@@ -21,11 +20,7 @@ async function fetchContent(page) {
 }
 
 export async function load({ url }) {
-	// Use params.page to get the dynamic part of the route, assuming your file structure matches your URL structure.
-	// e.g., for a URL like `/docs/about`, params.page would be `about`.
 	const myhtml = await fetchContent(url);
-
-	// Return the fetched HTML content as a prop to the component.
 	return {
 		props: {
 			myhtml: myhtml
