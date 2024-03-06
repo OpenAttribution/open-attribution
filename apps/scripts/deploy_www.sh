45e9479ba692a3d321b75643f8516bf0a2dfc5c9
james <jamesoclaire@gmail.com>
2024-03-06 13:05:41 -0800

    remove socket in favor of port

--- apps/scripts/deploy_www.sh
+++ apps/scripts/deploy_www.sh
@@ -18,7 +18,7 @@ Type=simple
 User=openattribution
 Group=openattribution
 SupplementaryGroups=nginx
-Environment=NODE_ENV=production SOCKET_PATH=/tmp/open-attribution-www.sock
+Environment=NODE_ENV=production PORT=4000
 ExecStartPre=/bin/bash -c "sudo rm -f /tmp/open-attribution-www.sock"
 ExecStart=/usr/bin/node /home/openattribution/open-attribution/apps/www/build
 ExecStartPost=/bin/bash -c "sleep 5 && sudo chown nginx:nginx /tmp/open-attribution-www.sock"
