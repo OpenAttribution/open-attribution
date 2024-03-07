457787c24495baf40bad053cca1904eaf23c0959
james <jamesoclaire@gmail.com>
2024-03-07 10:35:40 -0800

    Add vite configplugin to serve the static directory documentation/assets to docs/assets

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -2,21 +2,19 @@ import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+// Used for serving static mkbuild docs
+import express from 'express';
+const assets = express.static('./static/documentation/assets/');
+
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		purgeCss()
-		// I am unclear if this has ever helped worked, perhaps only after build?
-		// {
-		// 	name: 'docs-index',
-		// 	configureServer(server) {
-		// 		server.middlewares.use((req, res, next) => {
-		// 			if (req.url?.startsWith('/docs')) {
-		// 				req.url = req.url + '/index.html';
-		// 			}
-		// 			next();
-		// 		});
-		// 	}
-		// }
+		purgeCss(),
+		{
+		name: 'docs-static-assets',
+		configureServer(server) {
+    		server.middlewares.use('/docs/assets', assets)
+}
+}
 	]
 });
