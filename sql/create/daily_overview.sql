14fdaebd70a145c779edcbb0de0e0a33b412cdef
james <jamesoclaire@gmail.com>
2024-09-25 20:19:22 +0800

    rever sql

--- sql/create/daily_overview.sql
+++ sql/create/daily_overview.sql
@@ -1,17 +1,17 @@
 CREATE TABLE daily_overview
 (
     on_date Date,
-    store_id Lowcardinality (String),
-    network Lowcardinality (String),
-    campaign_name Lowcardinality (String),
-    campaign_id Lowcardinality (String),
-    ad_name Lowcardinality (String),
-    ad_id Lowcardinality (String),
-    impressions Simpleaggregatefunction (Sum, UInt64),
-    clicks Simpleaggregatefunction (Sum, UInt64),
-    installs Simpleaggregatefunction (Sum, UInt64),
+    store_id LowCardinality(String),
+    network LowCardinality(String),
+    campaign_name LowCardinality(String),
+    campaign_id LowCardinality(String),
+    ad_name LowCardinality(String),
+    ad_id LowCardinality(String),
+    impressions SimpleAggregateFunction(Sum, UInt64),
+    clicks SimpleAggregateFunction(Sum, UInt64),
+    installs SimpleAggregateFunction(Sum, UInt64),
     revenue Nullable (SimpleAggregateFunction (Sum, Decimal (38, 4))) DEFAULT 0
-) ENGINE = AGGREGATINGMERGETREE()
+) ENGINE = AggregatingMergeTree()
 ORDER BY (
     on_date, store_id, network, campaign_name, campaign_id, ad_name, ad_id
 );
