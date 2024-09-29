a4aac71c1df22bc22e94e69763f7233009abfc97
james <jamesoclaire@gmail.com>
2024-09-29 10:00:52 +0800

    Added multiple vscode workspaces due to root node_modules

--- apps/dash-frontend/src/routes/+page.svelte
+++ apps/dash-frontend/src/routes/+page.svelte
@@ -1,18 +1,10 @@
 <script lang="ts">
 	import Activity from 'lucide-svelte/icons/activity';
-	import CircleUser from 'lucide-svelte/icons/circle-user';
 	import CreditCard from 'lucide-svelte/icons/credit-card';
 	import DollarSign from 'lucide-svelte/icons/dollar-sign';
-	import Menu from 'lucide-svelte/icons/menu';
-	import Package2 from 'lucide-svelte/icons/package-2';
-	import Search from 'lucide-svelte/icons/search';
 	import Users from 'lucide-svelte/icons/users';
 
-	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
-	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-	import { Input } from '$lib/components/ui/input/index.js';
-	import * as Sheet from '$lib/components/ui/sheet/index.js';
 	import DateRangePicker from '$lib/DateRangePicker.svelte';
 	// import type { DateRange } from 'bits-ui';
 	import type { MyDateRange, OverviewEntries } from '../types';
@@ -48,6 +40,8 @@
 	let overviewData = $state(data.respData.overview);
 
 	let totalClicks = $state(handleUpdate(data.respData.overview));
+
+
 
 	let totalRevenue = $derived.by(() => {
 		// Check if overviewData is defined and not null
@@ -94,78 +88,7 @@
 </script>
 
 <div class="flex min-h-screen w-full flex-col">
-	<header class="bg-background sticky top-0 flex h-16 items-center gap-4 border-b px-4 md:px-6">
-		<nav
-			class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
-		>
-			<a href="##" class="flex items-center gap-2 text-lg font-semibold md:text-base">
-				<Package2 class="h-6 w-6" />
-				<span class="sr-only">Acme Inc</span>
-			</a>
-			<a href="##" class="text-foreground hover:text-foreground transition-colors"> Dashboard </a>
-			<a href="##" class="text-muted-foreground hover:text-foreground transition-colors">
-				Orders
-			</a>
-			<a href="##" class="text-muted-foreground hover:text-foreground transition-colors">
-				Products
-			</a>
-			<a href="##" class="text-muted-foreground hover:text-foreground transition-colors">
-				Customers
-			</a>
-			<a href="##" class="text-muted-foreground hover:text-foreground transition-colors">
-				Analytics
-			</a>
-		</nav>
-		<Sheet.Root>
-			<Sheet.Trigger asChild let:builder>
-				<Button variant="outline" size="icon" class="shrink-0 md:hidden" builders={[builder]}>
-					<Menu class="h-5 w-5" />
-					<span class="sr-only">Toggle navigation menu</span>
-				</Button>
-			</Sheet.Trigger>
-			<Sheet.Content side="left">
-				<nav class="grid gap-6 text-lg font-medium">
-					<a href="##" class="flex items-center gap-2 text-lg font-semibold">
-						<Package2 class="h-6 w-6" />
-						<span class="sr-only">Acme Inc</span>
-					</a>
-					<a href="##" class="hover:text-foreground"> Dashboard </a>
-					<a href="##" class="text-muted-foreground hover:text-foreground"> Orders </a>
-					<a href="##" class="text-muted-foreground hover:text-foreground"> Products </a>
-					<a href="##" class="text-muted-foreground hover:text-foreground"> Customers </a>
-					<a href="##" class="text-muted-foreground hover:text-foreground"> Analytics </a>
-				</nav>
-			</Sheet.Content>
-		</Sheet.Root>
-		<div class="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-			<form class="ml-auto flex-1 sm:flex-initial">
-				<div class="relative">
-					<Search class="text-muted-foreground absolute left-2.5 top-2.5 h-4 w-4" />
-					<Input
-						type="search"
-						placeholder="Search products..."
-						class="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-					/>
-				</div>
-			</form>
-			<DropdownMenu.Root>
-				<DropdownMenu.Trigger asChild let:builder>
-					<Button builders={[builder]} variant="secondary" size="icon" class="rounded-full">
-						<CircleUser class="h-5 w-5" />
-						<span class="sr-only">Toggle user menu</span>
-					</Button>
-				</DropdownMenu.Trigger>
-				<DropdownMenu.Content align="end">
-					<DropdownMenu.Label>My Account</DropdownMenu.Label>
-					<DropdownMenu.Separator />
-					<DropdownMenu.Item>Settings</DropdownMenu.Item>
-					<DropdownMenu.Item>Support</DropdownMenu.Item>
-					<DropdownMenu.Separator />
-					<DropdownMenu.Item>Logout</DropdownMenu.Item>
-				</DropdownMenu.Content>
-			</DropdownMenu.Root>
-		</div>
-	</header>
+	
 	<main class="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
 		<DateRangePicker onChange={handleDateChange} />
 		<div class="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
@@ -183,7 +106,7 @@
 				<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
 					<Card.Title class="text-sm font-medium">Clicks</Card.Title>
 					<CreditCard class="text-muted-foreground h-4 w-4" />
-				</Card.Header>
+				</Card.Header> {totalClicks} ->>
 				{#await data.respData}
 					Loading...
 				{:then mydatas}
@@ -196,11 +119,7 @@
 			<Card.Root>
 				<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
 					<Activity class="text-muted-foreground h-4 w-4" />
-					{#await data.respData}
-						Loading...
-					{:then mydata}
-						L is: {mydata.overview.length}
-					{/await}
+				Installs 	
 				</Card.Header>
 				<Card.Content>
 					<div class="text-2xl font-bold">{totalInstalls}</div>
@@ -213,7 +132,7 @@
 					<DollarSign class="text-muted-foreground h-4 w-4" />
 				</Card.Header>
 				<Card.Content>
-					<div class="text-2xl font-bold">{formattedRevenue}</div>
+					<div class="text-2xl font-bold">{totalRevenue} -> {formattedRevenue}</div>
 					<p class="text-muted-foreground text-xs">+20.1% from last month</p>
 				</Card.Content>
 			</Card.Root>
