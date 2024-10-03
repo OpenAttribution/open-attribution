c42d24e1d6a437752b05b9ff8e5681b02103ed81
james <jamesoclaire@gmail.com>
2024-10-03 17:28:41 +0800

    WIP: new form-submit on app page

--- apps/dash-frontend/src/routes/settings/apps/create/+page.server.ts
+++ apps/dash-frontend/src/routes/settings/apps/create/+page.server.ts
@@ -6,7 +6,7 @@ import { zod } from 'sveltekit-superforms/adapters';
 import { fail } from '@sveltejs/kit';
 
 export const actions = {
-	newAppCreate: async (event) => {
+	createApp: async (event) => {
 		const form = await superValidate(event, zod(formSchema));
 
 		if (!form.valid) {
@@ -14,6 +14,26 @@ export const actions = {
 				form
 			});
 		}
+
+		const store_id = form.data.storeId;
+		const app_name = form.data.appName;
+		const app_store = form.data.appStore;
+
+		console.log(`Create app Name: ${app_name}, ${app_store}, ${store_id}`);
+
+		const response = await fetch(
+			`http://dash-backend:8001/api/apps/${store_id}?app_name=${app_name}&store=${app_store}`,
+			{
+				method: 'POST'
+			}
+		);
+
+		// Check if the request was successful
+		if (!response.ok) {
+			console.error('Failed to add the app');
+			return { error: 'Failed to add the app' };
+		}
+
 		return {
 			form
 		};
--- apps/dash-frontend/src/routes/settings/apps/create/AddApp.svelte
+++ apps/dash-frontend/src/routes/settings/apps/create/AddApp.svelte
@@ -7,66 +7,29 @@
 	import { type SuperValidated, type Infer, superForm } from 'sveltekit-superforms';
 	import { zodClient } from 'sveltekit-superforms/adapters';
 
+	import { goto } from '$app/navigation';
+
 	export let data: SuperValidated<Infer<FormSchema>>;
 
 	const form = superForm(data, {
-		validators: zodClient(formSchema)
+		validators: zodClient(formSchema),
+
+		multipleSubmits: 'prevent'
 	});
 
 	const { form: formData, enhance } = form;
 
-	// import { enhance } from '$app/forms';
 	import { Button } from '$lib/components/ui/button';
+
+	async function handleSubmit(event: Event) {
+		event.preventDefault();
+
+		// After successful submission, redirect the user
+		goto('/new-page'); // Redirect to the new page
+	}
 </script>
 
-<!-- 
-<form method="POST" use:enhance>
-	<Form.Field {form} name="appName">
-		<Form.Control let:attrs>
-			<Form.Label>App Name</Form.Label>
-			<Input {...attrs} bind:value={$formData.appName} />
-		</Form.Control>
-		<Form.Description>Your app's display name.</Form.Description>
-		<Form.FieldErrors />
-	</Form.Field>
-	<Form.Field {form} name="storeId">
-		<Form.Control let:attrs>
-			<Form.Label>Store ID</Form.Label>
-			<Input {...attrs} bind:value={$formData.storeId} />
-		</Form.Control>
-		<Form.Description>
-			Store ID. iOS example: "1234567890" Android: "com.example.app"
-		</Form.Description>
-		<Form.FieldErrors />
-	</Form.Field>
-	<Form.Field {form} name="appStore">
-		<Form.Control let:attrs>
-			<Form.Label>App Store</Form.Label>
-			<Input {...attrs} bind:value={$formData.appStore} />
-			<RadioGroup.Root value="comfortable">
-				<div class="flex items-center space-x-2">
-					<RadioGroup.Item value="default" id="r1" />
-					<Label for="r1">Default</Label>
-				</div>
-				<div class="flex items-center space-x-2">
-					<RadioGroup.Item value="comfortable" id="r2" />
-					<Label for="r2">Comfortable</Label>
-				</div>
-				<div class="flex items-center space-x-2">
-					<RadioGroup.Item value="compact" id="r3" />
-					<Label for="r3">Compact</Label>
-				</div>
-				<RadioGroup.Input name="spacing" />
-			</RadioGroup.Root>
-		</Form.Control>
-		<Form.Description>Select which app store your app is live on.</Form.Description>
-		<Form.FieldErrors />
-	</Form.Field>
-	<Form.Button>Submit</Form.Button>
-</form>
- -->
-
-<form method="POST" use:enhance class="space-y-6">
+<form method="POST" use:enhance class="space-y-6" action="?/createApp">
 	<div class="grid grid-cols-1 md:grid-cols-2 gap-6">
 		<Form.Field {form} name="appName">
 			<Form.Control let:attrs>
