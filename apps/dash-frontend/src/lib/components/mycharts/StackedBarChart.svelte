a31745310534f81838c247f82d092650e9db0dc4
James O'Claire <jamesoclaire@gmail.com>
2024-10-22 23:16:26 +0800

    Working! Installs bar plot

---
+++ apps/dash-frontend/src/lib/components/mycharts/StackedBarChart.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { BarChart } from 'layerchart';
+
+	import {schemeAccent, schemeObservable10 } from 'd3-scale-chromatic';
+	import type { GroupedEntry } from '../../../types';
+
+  import { scaleOrdinal } from 'd3-scale';
+
+  const catppMocha = ['#f5e0dc','#cba6f7','#94e2d5','#f5c2e7','#f9e2af', '#74c7ec'];
+  // const edgyColors = ['#dc8a78','#8839ef','#179299','#ea76cb','#fe640b', '#209fb5'];
+  const keyColors = catppMocha
+	
+	// const keyColors = schemeObservable10
+
+
+  let { plotData = [] as GroupedEntry[] } = $props();
+
+  interface SeriesEntry {
+	key: string;
+	label: string;
+	color: string;
+}
+
+function generateSeriesKeys(
+	plotData: GroupedEntry[],
+	keyColors: string[]
+): SeriesEntry[] {
+  const uniqueKeys = Object.keys(plotData[0]).filter(key => key !== "on_date");
+	const seriesKeys = uniqueKeys.slice(0, keyColors.length).map((key, index) => ({
+		key,
+		label: key.charAt(0).toUpperCase() + key.slice(1),
+		color: keyColors[index],
+	}));
+
+	return seriesKeys;
+}
+
+const seriesKeys = generateSeriesKeys(plotData, keyColors);
+
+
+</script>
+
+
+ <div class="h-[300px] p-4 border rounded">
+  <BarChart
+    data={plotData}
+    x="on_date"
+	  series={seriesKeys}
+    seriesLayout="stack"
+    props={{
+      xAxis: { format: "none" },
+      yAxis: { format: "metric" },
+    }}
+    legend={{ placement: "top-right", classes: { root: "mt-2" } }}
+  />
+</div>
