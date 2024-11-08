03c78512745439b7b84082fb1b7fe7c53b47b3fb
James O'Claire <jamesoclaire@gmail.com>
2024-11-08 12:51:54 +0800

    WIP: Try tanstack

---
+++ apps/dash-frontend/src/lib/my-table/MyOverviewTable.svelte
@@ -0,0 +1,89 @@
+<script lang="ts" generics="TData, TValue">
+    import {
+     type ColumnDef,
+     type PaginationState,
+     getCoreRowModel,
+     getPaginationRowModel,
+    } from "@tanstack/table-core";
+
+import {
+    createSvelteTable,
+    FlexRender,
+  } from "$lib/components/ui/data-table/index.js";
+  import * as Table from "$lib/components/ui/table/index.js";
+ 
+
+    
+    type DataTableProps<TData, TValue> = {
+     data: TData[];
+     columns: ColumnDef<TData, TValue>[];
+    };
+
+    
+    let { data, columns }: DataTableProps<TData, TValue> = $props();
+    
+    let pagination = $state<PaginationState>({ pageIndex: 0, pageSize: 10 });
+    
+    const table = createSvelteTable({
+     get data() {
+      return data;
+     },
+     columns,
+     state: {
+      get pagination() {
+       return pagination;
+      },
+     },
+     onPaginationChange: (updater) => {
+      if (typeof updater === "function") {
+       pagination = updater(pagination);
+      } else {
+       pagination = updater;
+      }
+     },
+     getCoreRowModel: getCoreRowModel(),
+     getPaginationRowModel: getPaginationRowModel(),
+    });
+</script>
+
+
+<div class="rounded-md border">
+    <Table.Root>
+      <Table.Header>
+        {#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
+          <Table.Row>
+            {#each headerGroup.headers as header (header.id)}
+              <Table.Head>
+                {#if !header.isPlaceholder}
+                  <FlexRender
+                    content={header.column.columnDef.header}
+                    context={header.getContext()}
+                  />
+                {/if}
+              </Table.Head>
+            {/each}
+          </Table.Row>
+        {/each}
+      </Table.Header>
+      <Table.Body>
+        {#each table.getRowModel().rows as row (row.id)}
+          <Table.Row data-state={row.getIsSelected() && "selected"}>
+            {#each row.getVisibleCells() as cell (cell.id)}
+              <Table.Cell>
+                <FlexRender
+                  content={cell.column.columnDef.cell}
+                  context={cell.getContext()}
+                />
+              </Table.Cell>
+            {/each}
+          </Table.Row>
+        {:else}
+          <Table.Row>
+            <Table.Cell colspan={columns.length} class="h-24 text-center">
+              No results.
+            </Table.Cell>
+          </Table.Row>
+        {/each}
+      </Table.Body>
+    </Table.Root>
+  </div>
