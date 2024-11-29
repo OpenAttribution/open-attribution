9826082df2e5e4ebe159827249d77f9b4f06de56
James O'Claire <jamesoclaire@gmail.com>
2024-11-29 20:20:41 +0800

    Fix header in front of some svgs

--- apps/dash-frontend/src/routes/+layout.svelte
+++ apps/dash-frontend/src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 
 <ModeWatcher defaultMode={'dark'}></ModeWatcher>
 
-<header class="bg-background sticky top-0 flex h-16 items-center gap-4 border-b px-4 md:px-6">
+<header class="bg-background sticky top-0 z-30 flex h-16 items-center gap-4 border-b px-4 md:px-6">
 	<nav
 		class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
 	>
