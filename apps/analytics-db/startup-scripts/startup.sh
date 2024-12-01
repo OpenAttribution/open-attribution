e3b5300e8c0b29f9064a182567d4a98848a979eb
James O'Claire <jamesoclaire@gmail.com>
2024-12-01 14:12:41 +0800

    undo drop tables in startup script, can readd if helpful

--- apps/analytics-db/startup-scripts/startup.sh
+++ apps/analytics-db/startup-scripts/startup.sh
@@ -40,13 +40,6 @@ my_tables=(
 	"daily_overview_mv"
 )
 
-# Add drop tables section before creating new ones
-echo "Dropping existing tables..."
-for table in "${my_tables[@]}"; do
-	echo "Dropping table: $table"
-	clickhouse-client --query "DROP TABLE IF EXISTS $table"
-done
-
 # Loop through each SQL file in the directory and execute it
 for table in "${my_tables[@]}"; do
 	echo "Processing table: $table"
