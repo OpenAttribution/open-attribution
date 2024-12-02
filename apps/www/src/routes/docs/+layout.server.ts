e54eb02e394a5395cfb25b22d423da92271bf377
James O'Claire <jamesoclaire@gmail.com>
2024-12-02 13:55:22 +0800

    Clean up the layout functions a bit

--- apps/www/src/routes/docs/+layout.server.ts
+++ apps/www/src/routes/docs/+layout.server.ts
@@ -2,14 +2,14 @@ export const csr = false;
 
 import { PUBLIC_DOCS_HOST } from '$env/static/public';
 
-async function fetchContent(url) {
+async function fetchContent(url: URL) {
 	let htmlContent;
 	if (url && !url.pathname.includes('.')) {
-		// Ensure the path correctly points to where mkdocs HTML files are located.
+		// Ensure the path correctly points to where mkdocs HTML files were generated to.
 		// ie www/static/documentation/docs/index.html
 		const processedPathname = url.pathname.replace('/docs/', '');
 		var mypath = `${PUBLIC_DOCS_HOST}/generated-docs/${processedPathname}index.html`;
-		console.log(`Try rendering static path= ${mypath}`);
+		console.log(`Try rendering static docs path=${mypath}`);
 		const response = await fetch(mypath);
 		if (response.ok) {
 			htmlContent = await response.text();
@@ -17,7 +17,6 @@ async function fetchContent(url) {
 			htmlContent = 'Page not found.';
 		}
 	}
-	// console.log(`GOT CONTENT: ${htmlContent}`)
 	return htmlContent;
 }
 
