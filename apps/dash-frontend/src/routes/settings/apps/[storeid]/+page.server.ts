afa5f3f3fdeac00e0fc5d1f1e0f0045db59d2f79
James O'Claire <jamesoclaire@gmail.com>
2024-12-08 16:09:24 +0800

    Adding screen for adding a custom app_link for custom network

---
+++ apps/dash-frontend/src/routes/settings/apps/[storeid]/+page.server.ts
@@ -0,0 +1,48 @@
+import type { Actions, PageServerLoad } from './$types.js';
+
+export const actions = {
+	deleteApp: async ({ request }) => {
+		const data = await request.formData();
+		const id = data.get('id');
+
+		console.log(`Delete app id: ${id}`);
+
+		const response = await fetch(`http://dash-backend:8001/api/networks/${id}`, {
+			method: 'DELETE'
+		});
+
+		// Check if the request was successful
+		if (!response.ok) {
+			console.error('Failed to delete the app');
+			return { error: 'Failed to delete the app' };
+		}
+	}
+} satisfies Actions;
+
+export const load: PageServerLoad = async ({ params }) => {
+
+	const storeid = params.storeid;
+
+
+	return {
+		appData: fetch(`http://dash-backend:8001/api/apps/${storeid}`)
+			.then((resp) => {
+				if (resp.status === 200) {
+					return resp.json();
+				} else if (resp.status === 404) {
+					console.log('Not found');
+					return 'Not Found';
+				} else if (resp.status === 500) {
+					console.log('API Server error');
+					return 'Backend Error';
+				}
+			})
+			.then(
+				(json) => json,
+				(error) => {
+					console.log('Uncaught error', error);
+					return 'Uncaught Error';
+				}
+			)
+	};
+};
---
+++ apps/dash-frontend/src/routes/settings/apps/[storeid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	let { data } = $props();
+</script>
+
+{#await data.appData}
+	loading ...
+{:then app}
+	{console.log(app)}
+	<h1>App: {app.app.name}</h1>
+	<h1>StoreId: {app.app.store_id}</h1>
+	<h1>Store: {app.app.store}</h1>
+	<h1>Created: {app.app.created_at}</h1>
+	<h2 class="h2">App: {$page.params.storeid}</h2>
+{:catch}
+	<h1>App: Not Found</h1>
+	}
+{/await}
