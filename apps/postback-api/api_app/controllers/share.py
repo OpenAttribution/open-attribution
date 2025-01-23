d1482ce400b59963534070751e00e8cc432d8b57
James O'Claire <jamesoclaire@gmail.com>
2025-01-23 17:10:13 +0800

    Try HTML again 1s

--- apps/postback-api/api_app/controllers/share.py
+++ apps/postback-api/api_app/controllers/share.py
@@ -68,7 +68,7 @@ DETECT_APP_HTML = """
                     // If the app is not installed, redirect to the Play Store after a short delay
                     setTimeout(function() {{
                         window.location = `https://play.google.com/store/apps/details?id={google_store_id}`;
-                    }}, 15000); // Facebook pops up a question to leave Messenger, this delay is time that shows before redirec to store
+                    }}, 1000); // Facebook pops up a question to leave Messenger, this delay is time that shows before redirec to store
                 }}
 
                 // Call the function when the page loads
