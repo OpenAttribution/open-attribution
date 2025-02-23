0394f2c611f53f7a3aaf9b777b345005c6537c7b
James O'Claire <jamesoclaire@gmail.com>
2025-02-23 13:55:24 +0800

    Vite introduced strict host checking. I believe this should be fine as it is already checked by the proxy, but possibly should allow a variable here

--- apps/dash-frontend/vite.config.ts
+++ apps/dash-frontend/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
 	server: {
 		port: 5173,
 		host: '0.0.0.0',
-		strictPort: true // Prevent fallback to another port if this one is taken
+		strictPort: true, // Prevent fallback to another port if this one is taken
+		allowedHosts: true
 	}
 });
