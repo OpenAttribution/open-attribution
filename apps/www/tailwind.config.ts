ae9d4055845606fc9da32568c1bd94109936ae1d
James O'Claire <jamesoclaire@gmail.com>
2025-02-27 08:47:57 +0800

    Working upgrade to tailwind

--- apps/www/tailwind.config.ts
+++ apps/www/tailwind.config.ts
@@ -1,20 +1,13 @@
 import type { Config } from 'tailwindcss';
 
-import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
-import * as themes from '@skeletonlabs/skeleton/themes';
-
 export default {
 	darkMode: 'selector',
 
-	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: [
-		skeleton({
-			themes: [themes.nouveau]
-		})
-	]
+	plugins: []
 } satisfies Config;
