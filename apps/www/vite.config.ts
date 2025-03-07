7c494b91b0ad5a69c45d3d0d06ecb46036187776
James O'Claire <jamesoclaire@gmail.com>
2025-03-07 13:35:16 +0800

    move tailwindcss to top just in case

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -1,5 +1,5 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { myDocsAssetPlugin } from './mkdocs-static-asset-plugin';
 
